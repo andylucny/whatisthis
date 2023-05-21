df5bc8304bb75c262abe119e84a7b717e34f748f
Andrej Lucny <andylucny@gmail.com>
2023-05-21 19:03:50 +0200

    downloader of models

---
+++ iCub_whisper/download.py
@@ -0,0 +1,34 @@
+print('please, wait...')
+import requests
+import os
+from zipfile import ZipFile
+from io import BytesIO
+import whisper
+
+def download_espeak():
+    if os.path.exists("espeak.exe"):
+        return
+    print("downloading espeak")
+    url = "https://www.agentspace.org/download/espeak.zip"
+    response = requests.get(url)
+    zip = ZipFile(BytesIO(response.content))
+    zip.extractall()
+    print("espeak downloaded")
+
+def download_DinoViT_model():
+    if os.path.exists("dino_deits8.onnx"):
+        return
+    print("downloading model")
+    url = "https://dl.fbaipublicfiles.com/dino/dino_deitsmall8_pretrain/dino_deits8.onnx"
+    response = requests.get(url)
+    open("dino_deits8.onnx","wb").write(response.content)
+    print("model downloaded")
+
+def download_whisper():    
+    whisper.load_model("base.en")
+
+if __name__ == "__main__":
+    download_espeak()
+    download_DinoViT_model()
+    download_whisper()
+    print("done")
--- iCub_whisper/run.bat
+++ iCub_whisper/run.bat
@@ -1,3 +1,4 @@
 set PATH=.;iCubSim\bin;%PATH%
+rem python download.py
 python -i main.py
 pause
