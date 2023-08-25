a4ccd7cea36786f216b5a9ae46fced153e7ecc1f
Andrej Lucny <andylucny@gmail.com>
2023-08-25 11:19:23 +0200

    download the robot config

--- Nico_demo/download.py
+++ Nico_demo/download.py
@@ -15,6 +15,9 @@ def download_and_save(url,path):
 def download_DinoViT_model():
     download_and_save("https://www.agentspace.org/download/dino_deits8-224-final.onnx","dino_deits8.onnx")
 
+def download_robot_config():
+    download_and_save("https://www.agentspace.org/download/nico_humanoid_upper_rh7d_ukba.json","nico_humanoid_upper_rh7d_ukba.json")
+
 def download_v4l2(path,url):
     if os.path.exists(path):
         return
@@ -27,6 +30,7 @@ def download_v4l2(path,url):
 
 def download_all():
     download_DinoViT_model()
+    download_robot_config()
     download_v4l2("v4l2-ctl.exe","https://www.agentspace.org/download/v4l2-ctl.zip")  
 
 if __name__ == "__main__":
